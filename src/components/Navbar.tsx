0b4b7d09ae9a56e0c2830a6208a5e8324e993eee
prasanth <prashanth9.namala@gmail.com>
2026-03-17 15:26:28 +0530

    Implemented Dark Mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { ColorModeButton } from "@/components/ui/color-mode";
 
 const Navbar = () => {
     return (<>
     <HStack>
-        <Image src={logo} alt="logo" boxSize="50px"/>
+       <ColorModeButton />
         <Text><b>Gamehub</b></Text>
     </HStack>
     </>)
