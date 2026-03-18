b792c11f8913d1ac5cc82a57653fe8a2537155a8
prasanth <prashanth9.namala@gmail.com>
2026-03-18 13:11:58 +0530

    Implemented Dark and light mode with a switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,21 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Switch } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
-import { ColorModeButton } from "@/components/ui/color-mode";
+import { useColorMode } from "@/components/ui/color-mode";
 
 const Navbar = () => {
-    return (<>
-    <HStack>
-       <ColorModeButton />
-        <Text><b>Gamehub</b></Text>
-    </HStack>
-    </>)
-}
+  const { toggleColorMode } = useColorMode();
+  return (
+    <>
+      <HStack justifyContent="space-between" padding="10px">
+        <Image src={logo} alt="logo" boxSize="50px" />
+        <Switch.Root key="md" size="md" onCheckedChange={toggleColorMode} colorPalette="teal">
+          <Switch.HiddenInput />
+          <Switch.Control />
+          <Switch.Label>Dark Mode</Switch.Label>
+        </Switch.Root>
+      </HStack>
+    </>
+  );
+};
 
 export default Navbar;
