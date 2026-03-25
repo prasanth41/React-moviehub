d4ba3ab3d48ed5359fb73b9360359df00345c774
prasanth <prashanth9.namala@gmail.com>
2026-03-25 18:45:56 +0530

    Fixed Style issues in GenreList

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,7 +1,7 @@
 import useData from "@/hooks/useData";
 import useGenres, { Genre } from "@/hooks/useGenres";
 import getCroppedImageurl from "@/services/image-url";
-import { Button, HStack, Image, List, Spinner, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, HStack, Image, List, Spinner, Text } from "@chakra-ui/react";
 
 interface Props {
     onChangeGenre: (genre: Genre) => void;
@@ -11,18 +11,20 @@ const GenreList = ({onChangeGenre, selectedGenre}: Props) => {
   const { data, isLoading , error} = useGenres();
   if (isLoading) return <Spinner />;
   if (error) return null;
-  return (
-      <List.Root listStyleType="none" >
+  return (<>
+  <Heading fontSize="2xl" marginBottom={3} >Genres</Heading>
+        <List.Root listStyleType="none" >
         {data.map((genre) => (
           <List.Item key={genre.id} paddingY="5px">
-            <HStack>
-              <Image boxSize="32px" borderRadius={8} padding={1} src={getCroppedImageurl(genre.image_background)} objectFit="cover" />
-              <Button  onClick={()=> onChangeGenre(genre)} fontSize="lg" variant= 'plain'
-              fontWeight={ genre.id == selectedGenre?.id ? "bold" : "normal"} colorPalette={ genre.id == selectedGenre?.id ? "yellow" : ''}>{genre.name}</Button>
+            <HStack >
+              <Image boxSize="32px" objectFit="cover" borderRadius={8}  src={getCroppedImageurl(genre.image_background)} />
+              <Box as="button" cursor="pointer" onClick={()=> onChangeGenre(genre)} fontSize="lg" whiteSpace="normal" textAlign="left"
+              fontWeight={ genre.id == selectedGenre?.id ? "bold" : "normal"} color={ genre.id == selectedGenre?.id ? "yellow" : ''}>{genre.name}</Box>
             </HStack>
             </List.Item>
         ))}
       </List.Root>
+  </>
   );
 };
 
