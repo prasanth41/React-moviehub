fc5d952549fad4edbff09c99519bb3d154b68d70
prasanth <prashanth9.namala@gmail.com>
2026-03-31 20:36:39 +0530

    Fixed type issue

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -13,6 +13,6 @@ const useGenres = () => useQuery({
         queryKey: ["genres"],
         queryFn: apiClient.getAll,
         staleTime: 24 * 60 * 60 * 1000, // 24 hours
-        initialData: { count: genres.length, results: genres }
+        initialData: { count: genres.length, results: genres, next: null }
     });  
 export default useGenres;
